9cda3228b4f7b7aa8e3c44842dd231ed4e430d25
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-01-31 12:15:35 -0800

    major version

--- python/sqlite_ulid/__init__.py
+++ python/sqlite_ulid/__init__.py
@@ -1,8 +1,19 @@
 import os
 import sqlite3
+import platform 
+
+system = platform.system()
+
+_extension_name = None
+if system == 'Darwin':
+  _extension_name = "ulid0.dylib"
+elif system == 'Windows':
+  _extension_name = "ulid0.dll"
+else: 
+  _extension_name = "ulid0.so"
 
 def loadable_path():
-  loadable_path = os.path.join(os.path.dirname(__file__), "ulid")
+  loadable_path = os.path.join(os.path.dirname(__file__), _extension_name)
   return os.path.normpath(loadable_path)
 
 def load(conn: sqlite3.Connection)  -> None:
