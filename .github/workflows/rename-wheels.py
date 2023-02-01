3e6a1201dc916d44718d4d154a72fa6531450629
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-01-31 21:28:43 -0800

    proper rename

--- .github/workflows/rename-wheels.py
+++ .github/workflows/rename-wheels.py
@@ -7,9 +7,9 @@ is_macos_arm_build = '--is-macos-arm' in sys.argv
 
 for filename in os.listdir(wheel_dir):
   filename = Path(wheel_dir, filename)
-  if not filename.suffix == 'whl':
+  if not filename.suffix == '.whl':
     continue
-  new_filename = (filename
+  new_filename = (filename.name
     .replace('cp311-cp311', 'py3-none')
     .replace('linux_x86_64', 'manylinux_2_17_x86_64.manylinux2014_x86_64')
     
@@ -20,4 +20,4 @@ for filename in os.listdir(wheel_dir):
   else:
     new_filename = new_filename.replace('macosx_12_0_universal2', 'macosx_10_6_x86_64')
   
-  os.rename(filename, new_filename)
+  os.rename(filename, Path(wheel_dir, new_filename))
--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 import os
 import platform
 
-VERSION = "0.0.1.alpha-3"
+VERSION = "0.0.1.alpha-4"
 
 def determine_package_data():
   machine = platform.machine()
